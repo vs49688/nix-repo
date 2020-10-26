d221a90c95677c0addfc8d042db5129108b81e5e
Zane van Iperen <z.vaniperen@uq.edu.au>
2020-10-26 15:23:25 +1000

    containers/portal-client: cleanups

--- containers/portal-client/default.nix
+++ containers/portal-client/default.nix
@@ -5,19 +5,31 @@
 }:
 let
   pkg = portal-client;
+  etcDir = "${pkg}/usr/share/${pkg.pname}/etc/${pkg.pname}";
 in
 dockerTools.buildImage {
   name = "uq-rcc/${pkg.pname}";
   tag  = pkg.version;
 
-  contents = [ tini pkg ];
-
   runAsRoot = ''
     #!{pkgs.runtimeShell}
+    ${dockerTools.shadowSetup}
     mkdir /tmp
+    groupadd -r ${pkg.pname}
+    useradd -r ${pkg.pname}
+    mkdir /config
+    cp ${etcDir}/application.yml /config
+    chmod 0600 /config/application.yml
+    chown -R ${pkg.pname}:${pkg.pname} /config
   '';
 
   config = {
-    Cmd = [ "${tini}/bin/tini" "--" "${pkg}/bin/portal-client" ];
+    Cmd = [
+      "${tini}/bin/tini" "--"
+      "${pkg}/bin/${pkg.pname}" "--spring.config.location=/config/application.yml"
+    ];
+    Volumes = {
+      "/config" = {};
+    };
   };
 }
