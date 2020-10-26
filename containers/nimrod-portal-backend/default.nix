c1d5a2b2d058235567d9e8fb326528aa9356bc1e
Zane van Iperen <z.vaniperen@uq.edu.au>
2020-10-26 15:23:07 +1000

    containers/nimrod-portal-backend: cleanups

--- containers/nimrod-portal-backend/default.nix
+++ containers/nimrod-portal-backend/default.nix
@@ -4,20 +4,32 @@
 , nimrod-portal-backend
 }:
 let
-  pkg = nimrod-portal-backend;
+  pkg    = nimrod-portal-backend;
+  etcDir = "${pkg}/usr/share/${pkg.pname}/etc/${pkg.pname}";
 in
 dockerTools.buildImage {
   name = "uq-rcc/${pkg.pname}";
   tag  = pkg.version;
 
-  contents = [ tini pkg ];
-
   runAsRoot = ''
     #!{pkgs.runtimeShell}
+    ${dockerTools.shadowSetup}
     mkdir /tmp
+    groupadd -r ${pkg.pname}
+    useradd -r ${pkg.pname}
+    mkdir /config
+    cp ${etcDir}/application.yml /config
+    chmod 0600 /config/application.yml
+    chown -R ${pkg.pname}:${pkg.pname} /config
   '';
 
   config = {
-    Cmd = [ "${tini}/bin/tini" "--" "${pkg}/bin/nimrod-portal-backend" ];
+    Cmd = [
+      "${tini}/bin/tini" "--"
+      "${pkg}/bin/${pkg.pname}" "--spring.config.location=/config/application.yml"
+    ];
+    Volumes = {
+      "/config" = {};
+    };
   };
 }
