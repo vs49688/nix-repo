47ad843cb17a07c555a33fa52d09c2731cf3be3f
Zane van Iperen <z.vaniperen@uq.edu.au>
2021-06-11 13:37:59 +1000

    nimrod-portal: 1.2.1 -> 1.2.2

--- pkgs/nimrod-portal/default.nix
+++ pkgs/nimrod-portal/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 stdenv.mkDerivation rec {
   pname   = "nimrod-portal";
-  version = "1.2.1";
+  version = "1.2.2";
 
   src = fetchurl {
-    url    = "https://github.com/UQ-RCC/nimrod-portal/releases/download/1.2.1/nimrod-portal-1.2.1.tar.gz";
-    sha256 = "0csyf3wnjhlia1l5vl7snln1bx6hs87jg7g63hyi0g6pijwz80ci";
+    url    = "https://github.com/UQ-RCC/nimrod-portal/releases/download/${version}/nimrod-portal-${version}.tar.gz";
+    sha256 = "0mf6kz5iwfs5sz08v5yhw5fhq25y1ra8r452lwqbxwaxk3jiy894";
   };
 
   dontConfigure = true;
@@ -14,10 +14,10 @@ stdenv.mkDerivation rec {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out
-    mv * $out
-    find $out -type d -print0 | xargs -0 chmod 0755
-    find $out -type f -print0 | xargs -0 chmod 0644
+    mkdir -p $out/share/${pname}
+    mv * $out/share/${pname}
+    find $out/share/${pname} -type d -print0 | xargs -0 chmod 0755
+    find $out/share/${pname} -type f -print0 | xargs -0 chmod 0644
 
     runHook postInstall
   '';
