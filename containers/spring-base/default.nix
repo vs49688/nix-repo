f720cb550abf9a2f319100191d0ef31033b03ed8
Zane van Iperen <z.vaniperen@uq.edu.au>
2021-06-15 11:58:58 +1000

    containers/spring-base: rewrite to use buildLayeredImage

--- containers/spring-base/default.nix
+++ containers/spring-base/default.nix
@@ -6,29 +6,33 @@
 , args    ? []
 , uid     ? 1000
 , gid     ? 1000
+, extra   ? []
 }:
 let
   etcDir = "${pkg}/usr/share/${pkg.pname}/etc/${pkg.pname}";
 in
-with pkgs; dockerTools.buildImage {
+with pkgs; dockerTools.buildLayeredImage {
   inherit name;
   inherit tag;
 
-  contents = [ tini busybox pkg ];
+  contents = [ tini busybox pkg ] ++ extra;
 
-  runAsRoot = let
+  fakeRootCommands = let
     uidstr = toString uid;
     gidstr = toString gid;
   in ''
-    #!{pkgs.runtimeShell}
-    ${dockerTools.shadowSetup}
-    mkdir /tmp
-    groupadd -r -g "${uidstr}" "${pkg.pname}"
-    useradd  -r -g "${gidstr}" -u "${uidstr}" "${pkg.pname}"
-    mkdir /config
-    cp ${etcDir}/application.yml /config
-    chmod 0600 /config/application.yml
-    chown -R "${uidstr}:${gidstr}" /config
+    mkdir -p ./tmp ./etc
+
+    printf 'root:x:0:0::/root:${busybox}/bin/ash\n' > ./etc/passwd
+    printf '${pkg.pname}:x:${uidstr}:${gidstr}::/home/${pkg.pname}:\n' >> ./etc/passwd
+
+    printf 'root:x:0:\n' > ./etc/group
+    printf '${pkg.pname}:x:${uidstr}:\n' >> ./etc/group
+
+    mkdir -p ./config ./home/${pkg.pname}
+    cp ${etcDir}/application.yml ./config
+    chmod 0600 ./config/application.yml
+    chown -R "${uidstr}:${gidstr}" ./config ./home/${pkg.pname}
   '';
 
   config = {
