2244d6819dbf5f9aeaa24082fab684f8a87cdbf9
Zane van Iperen <zane@zanevaniperen.com>
2021-09-15 21:43:49 +1000

    jdownloader: checkout svn components individually

--- pkgs/jdownloader/default.nix
+++ pkgs/jdownloader/default.nix
@@ -1,9 +1,23 @@
 { stdenv, lib, fetchsvn, ant, jdk8, jre8, makeDesktopItem, desktop-file-utils, imagemagick, makeWrapper }:
 let
+  svnRevision = "44937";
+
   appWorkUtilsSrc = fetchsvn {
     url    = "svn://svn.appwork.org/utils";
     rev    = "3570";
     sha256 = "0h0p0j3r7fdg3vjggwsx8lav4w4jm02ci233wld4irzm6qmr5f6i";
+  };
+
+  jdbrowserSrc = fetchsvn {
+    url    = "svn://svn.jdownloader.org/jdownloader/browser";
+    rev    = svnRevision;
+    sha256 = "0nsavm4gnq19hgrmak5knpnw1gl8d3m39fq8fm9wv62k05xz9n2r";
+  };
+
+  myJDownloaderSrc = fetchsvn {
+    url    = "svn://svn.jdownloader.org/jdownloader/MyJDownloaderClient";
+    rev    = svnRevision;
+    sha256 = "19b1h52lwykz4ksljfa1rrvcjr5k1kmlf5d9favmm403sfngm3m4";
   };
 
   description = "JDownloader is a free, open-source download management tool";
@@ -16,17 +30,15 @@ let
     desktopName = "JDownloader";
     categories  = "Network;FileTransfer;";
   };
-
-  svnRevision = "44937";
 in
 stdenv.mkDerivation rec {
   pname   = "jdownloader";
   version = "2.${svnRevision}";
 
   src = fetchsvn {
-    url    = "svn://svn.jdownloader.org/jdownloader";
+    url    = "svn://svn.jdownloader.org/jdownloader/trunk";
     rev    = svnRevision;
-    sha256 = "1h45nc3hnn11lmv6qg51mf0lkxc3f8r7m1fqhmkwvmi7aq7n4w0b";
+    sha256 = "12nlqi6lmx6lmajr37h3nli56lg6hqi4pnk8lchgi204i5paj0bh";
   };
 
   nativeBuildInputs = [ ant imagemagick desktop-file-utils makeWrapper ];
@@ -45,13 +57,12 @@ stdenv.mkDerivation rec {
   '';
 
   buildPhase = ''
-    cd trunk
     ln build/newBuild/build_standalone.xml build.xml
 
     ${ant}/bin/ant \
-      -Ddep.awu=../awu \
-      -Ddep.browser=${src}/browser \
-      -Ddep.myjd=${src}/MyJDownloaderClient
+      -Ddep.awu=./awu \
+      -Ddep.browser=${jdbrowserSrc} \
+      -Ddep.myjd=${myJDownloaderSrc}
   '';
 
   installPhase = ''
@@ -64,7 +75,7 @@ stdenv.mkDerivation rec {
       --add-flags "-cp $out/jdownloader/JDownloader.jar org.jdownloader.launcher.JDLauncher"
 
     convert -resize 256x256 \
-      "${src}/trunk/artwork/icons/by TRazo/JDownloader/JDownloader.png" \
+      "${src}/artwork/icons/by TRazo/JDownloader/JDownloader.png" \
       $out/share/icons/hicolor/256x256/apps/jdownloader.png
 
     ${desktopItem.buildCommand}
