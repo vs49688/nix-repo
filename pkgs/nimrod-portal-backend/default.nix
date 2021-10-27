6ef737165b38f6ad08f6d23b184d7d3aa02154b8
Zane van Iperen <zane@zanevaniperen.com>
2021-10-27 17:21:21 +1000

    nimrod-portal-backend: cleanups

--- pkgs/nimrod-portal-backend/default.nix
+++ pkgs/nimrod-portal-backend/default.nix
@@ -1,23 +1,25 @@
 { stdenv, lib, fetchurl, jre, busybox, makeWrapper }:
-stdenv.mkDerivation rec {
+let
   pname   = "nimrod-portal-backend";
   version = "1.13.1";
+in stdenv.mkDerivation {
+  inherit pname version;
 
   src = fetchurl {
-    url    = "https://github.com/UQ-RCC/nimrodg/releases/download/${version}/nimrod-portal-backend-${version}.tar.gz";
+    url    = "https://github.com/UQ-RCC/nimrodg/releases/download/${version}/${pname}-${version}.tar.gz";
     sha256 = "1k13w1s19n5f7dp2wc977l7zz1chk9gchr46n0wk1bmady35s199";
   };
 
   buildInputs = [ makeWrapper ];
 
   installPhase = ''
-    dest="$out/usr/share/nimrod-portal-backend"
+    dest="$out/usr/share/${pname}"
     mkdir -p "$dest"
     cp -ra . "$dest"
 
     mkdir -p "$out/bin"
-    makeWrapper "$dest/bin/nimrod-portal-backend" \
-        "$out/bin/nimrod-portal-backend" \
+    makeWrapper "$dest/bin/${pname}" \
+        "$out/bin/${pname}" \
         --prefix PATH : ${lib.makeBinPath [ jre busybox ]} \
         --set JAVA_HOME ${jre.home}
   '';
