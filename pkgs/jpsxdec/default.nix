f794d38de580590d3feec4ff8a2440f16ffb8194
Zane van Iperen <zane@zanevaniperen.com>
2021-11-16 21:05:29 +1000

    jpsxdec: use upstream PR
    https://github.com/NixOS/nixpkgs/pull/144454

--- pkgs/jpsxdec/default.nix
+++ pkgs/jpsxdec/default.nix
@@ -7,37 +7,37 @@
 , unoconv
 , makeWrapper
 , makeDesktopItem
+, copyDesktopItems
 }:
-let
-  pname   = "jpsxdec";
+stdenv.mkDerivation rec {
+  pname = "jpsxdec";
   version = "1.05";
 
-  description = "cross-platform PlayStation 1 audio and video converter";
-
-  desktopItem = makeDesktopItem {
-    name        = "jPSXdec";
-    exec        = pname;
-    icon        = pname;
-    comment     = description;
-    desktopName = pname;
-    categories  = "AudioVideo;Utility;";
-  };
-in stdenv.mkDerivation rec {
-  inherit pname;
-  inherit version;
+  description = "Cross-platform PlayStation 1 audio and video converter";
 
   src = fetchFromGitHub {
-    owner  = "m35";
-    repo   = pname;
-    rev    = "v${version}";
+    owner = "m35";
+    repo = pname;
+    rev = "v${version}";
     sha256 = "0wnfvvcyldf699b08lzlc0gshl7rn09a6q4i7jmr41izlcdszdbz";
   };
 
-  nativeBuildInputs = [ ant jdk unoconv makeWrapper ];
+  nativeBuildInputs = [ ant jdk unoconv makeWrapper copyDesktopItems ];
   buildInputs = [ jre ];
 
   patches = [
     ./0001-jpsxdec-hackfix-build-with-newer-JDKs.patch
+  ];
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = pname;
+      exec = pname;
+      icon = pname;
+      comment = description;
+      desktopName = "jPSXdec";
+      categories = "AudioVideo;Utility;";
+    })
   ];
 
   buildPhase = ''
@@ -61,18 +61,18 @@ in stdenv.mkDerivation rec {
     makeWrapper ${jre}/bin/java $out/bin/jpsxdec \
       --add-flags "-jar $out/jpsxdec/jpsxdec.jar"
 
-    cp "${src}/jpsxdec/src/jpsxdec/gui/icon48.png" $out/share/pixmaps/${pname}.png
-    ln -s "${desktopItem}/share/applications" $out/share
+    cp ${src}/jpsxdec/src/jpsxdec/gui/icon48.png $out/share/pixmaps/${pname}.png
 
     runHook postInstall
   '';
 
   meta = with lib; {
     inherit description;
-    homepage    = "http://jpsxdec.blogspot.com/";
-    platforms   = platforms.all;
-    license     = {
-      url       = "https://raw.githubusercontent.com/m35/jpsxdec/readme/.github/LICENSE.md";
+    homepage = "https://jpsxdec.blogspot.com/";
+    platforms = platforms.all;
+    license = {
+      url = "https://raw.githubusercontent.com/m35/jpsxdec/readme/.github/LICENSE.md";
+      free = true;
     };
     maintainers = with maintainers; [ zane ];
   };
