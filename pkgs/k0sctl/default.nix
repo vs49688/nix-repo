8efcc1d7dda0ebd66c3b51a4388155cff6795df6
Zane van Iperen <zane@zanevaniperen.com>
2021-11-30 17:11:52 +1000

    k0sctl: buildFlagsArray -> ldflags

--- pkgs/k0sctl/default.nix
+++ pkgs/k0sctl/default.nix
@@ -14,12 +14,11 @@ in buildGoModule {
 
   vendorSha256 = "049l9n0aja0xj6qqk2v2y411sim3xddq1w08ywp7sj4sd1jhx9hq";
 
-  buildFlagsArray = ''
-    -ldflags=
-    -X github.com/k0sproject/k0sctl/version.Environment=production
-    -X github.com/k0sproject/k0sctl/version.GitCommit=v${version}
-    -X github.com/k0sproject/k0sctl/version.Version=v${version}
-  '';
+  ldflags = [
+    "-X" "github.com/k0sproject/k0sctl/version.Environment=production"
+    "-X" "github.com/k0sproject/k0sctl/version.GitCommit=v${version}"
+    "-X" "github.com/k0sproject/k0sctl/version.Version=v${version}"
+  ];
 
   meta = with lib; {
     description = "A bootstrapping and management tool for k0s clusters";
