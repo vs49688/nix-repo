7c3e4070581e93255e8f709edb436fb75b65565d
Zane van Iperen <zane@zanevaniperen.com>
2021-12-04 13:53:58 +1000

    tini: remove
    The upstream version is now suitable.

--- default.nix
+++ default.nix
@@ -12,18 +12,9 @@ let
     callPackage = lib.callPackage;
     callPackages = lib.callPackages;
   } // ((import ./overlay.nix) self pkgs) // {
-    ##
-    # Can't put this in the overlay until after 21.05.
-    # - Upstream tini depends directly on glibc which is propagated
-    #   to other things an breaks builds.
-    ##
-    tini = lib.callPackage ./pkgs/tini { };
-
     pkgsStatic = pkgs.pkgsStatic // rec {
       callPackage = pkgs.lib.callPackageWith (pkgs.pkgsStatic // self.pkgsStatic);
       callPackages = pkgs.lib.callPackagesWith (pkgs.pkgsStatic // self.pkgsStatic);
-
-      tini = callPackage ./pkgs/tini { cmake = pkgs.cmake; };
     };
 
     pkgs = pkgs // self;
