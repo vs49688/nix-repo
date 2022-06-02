7da50b3e9cb582d61c16f67aa4f077fbd2f5110e
Zane van Iperen <zane@zanevaniperen.com>
2022-06-02 22:51:54 +1000

    navidrome-mbz: unstable-2021-12-28 -> unstable-2022-06-01
    Refactors the module slightly. Adds the appropriate version
    strings to the executable, with a -mbz suffix to differentiate
    it from Navidrome proper, and moves the UI to its own derivation
    to make life simpler when editing the derivation.

--- pkgs/navidrome-mbz/default.nix
+++ pkgs/navidrome-mbz/default.nix
@@ -1,10 +1,13 @@
 { stdenv, pkgs, lib, buildGoModule, fetchFromGitHub, pkg-config, zlib, taglib, nodejs }:
 let
+  pname = "navidrome-mbz";
+  version = "unstable-2022-06-01";
+
   src = fetchFromGitHub {
     owner = "vs49688";
     repo = "navidrome";
-    rev = "221763ff4b6ffcac553ea11cb8ff606e4572dd43";
-    sha256 = "sha256-FgTxi4w46bdKNYIOQYMWlBhJYVRnnZElMkMuxcLn2L0=";
+    rev = "be61bcdf98b3cd8cf3a4f398287eb93022ec66dc";
+    sha256 = "sha256-sw/PQzo9B+JOzkKVYXoIkNOXRzVIL2fq8iUQQBzO0PQ=";
   };
 
   nodeDependencies = ((import ./node-composition.nix {
@@ -13,12 +16,42 @@ let
   }).nodeDependencies.override (old: {
     src = "${src}/ui";
   }));
+
+  ui = stdenv.mkDerivation {
+    inherit version src;
+
+    pname = "${pname}-ui";
+
+    sourceRoot = "source/ui";
+
+    nativeBuildInputs = [
+      nodejs
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+
+      cp -R ${nodeDependencies}/lib/node_modules ./node_modules
+      find node_modules -type d -print0 | xargs -0 chmod 0755
+      npm install
+      npm run check-formatting
+      npm run lint
+      npm run build
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mv build $out
+
+      runHook postInstall
+    '';
+  };
 in
 buildGoModule {
-  inherit src;
-
-  pname = "navidrome-mbz";
-  version = "unstable-2021-12-28";
+  inherit pname version src;
 
   vendorSha256 = "sha256-xMAxGbq2VSXkF9R9hxB9EEk2CnqsRxg2Nmt7zyXohJI=";
 
@@ -32,21 +65,18 @@ buildGoModule {
     taglib
   ];
 
+  ldflags = [
+    "-s" "-w"
+    "-X github.com/navidrome/navidrome/consts.gitSha=${lib.substring 0 7 src.rev}"
+    "-X github.com/navidrome/navidrome/consts.gitTag=v0.47.5-mbz-SNAPSHOT"
+  ];
+
   passthru = {
-    inherit nodeDependencies;
+    inherit ui nodeDependencies;
   };
 
   preBuild = ''
-    pushd ui
-
-    cp -R ${nodeDependencies}/lib/node_modules ./node_modules
-    find node_modules -type d -print0 | xargs -0 chmod 0755
-    npm install
-    npm run check-formatting
-    npm run lint
-    npm run build
-
-    popd
+    cp -R ${ui} ui/build
   '';
 
   preCheck = ''
