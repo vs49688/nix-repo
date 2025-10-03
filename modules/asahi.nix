0d67dd7ee9d6a2ce2a9acb799b70c0e3c6152e3e
Zane van Iperen <zane@zanevaniperen.com>
2025-10-03 23:19:25 +1000

    modules/asahi: move set-battery-threshold script here

--- modules/asahi.nix
+++ modules/asahi.nix
@@ -1,6 +1,33 @@
-{ ... }: {
+{ pkgs, utils, ... }: {
   boot.kernelParams = [
     # Make the function keys function.
     "hid_apple.fnmode=2"
   ];
+
+  systemd.services.set-battery-threshold = {
+    description = "set battery charge threshold";
+
+    confinement.enable = true;
+    confinement.mode = "full-apivfs";
+    confinement.binSh = null;
+
+    wantedBy = [ "multi-user.target" ];
+
+    unitConfig.ConditionPathExists = "/sys/class/power_supply/macsmc-battery/charge_control_end_threshold";
+
+    serviceConfig.ExecStart = let
+      args = [
+        "-c"
+        "echo 80 > /sys/class/power_supply/macsmc-battery/charge_control_end_threshold"
+      ];
+    in ''
+      ${pkgs.bash}/bin/sh ${utils.escapeSystemdExecArgs args}
+    '';
+
+    serviceConfig.Type = "oneshot";
+    serviceConfig.User = "root";
+    serviceConfig.Group = "root";
+    serviceConfig.RemainAfterExit = true;
+    serviceConfig.BindPaths = [ "/sys/class/power_supply/macsmc-battery" ];
+  };
 }
