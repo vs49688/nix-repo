a69fae1d6d151f278ad28f8770985f1fbdc66aa9
Zane van Iperen <zane@zanevaniperen.com>
2025-11-03 02:15:21 +1000

    epsonscan2-v850: switch to overrideAttrs, update pname

--- pkgs/epsonscan2-v850/default.nix
+++ pkgs/epsonscan2-v850/default.nix
@@ -1,6 +1,8 @@
 { epsonscan2 }:
-epsonscan2.overrideDerivation(old: {
-  patches = old.patches ++ [
+epsonscan2.overrideAttrs(final: prev: {
+  pname = "${prev.pname}-v850";
+
+  patches = prev.patches ++ [
     ./0005-Fix-crash-no-serial-number.patch
     ./0001-Controller-UsbFinder-add-Perfection-V850-Pro-V800-Ph.patch
   ];
