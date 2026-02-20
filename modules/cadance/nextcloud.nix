401f1513d5223ca09c3d99b502732b63c6b95665
Zane van Iperen <zane@zanevaniperen.com>
2026-02-20 20:52:32 +1000

    modules/cadance/nextcloud: extract from private config

--- modules/cadance/nextcloud.nix
+++ modules/cadance/nextcloud.nix
@@ -1,157 +1,232 @@
-##
-# CADANCE module to run a Nextcloud container.
-##
-{ config, pkgs, lib, ... }:
+{ lib, config, pkgs, ... }:
 let
-  cfg = config.cadance.containers.nextcloud;
-in {
-  options.cadance.containers.nextcloud = with lib; {
-    enable = mkOption {
-      default = false;
-      type = types.bool;
-      description = "Enable NextCloud container";
-    };
+  cfg = config.cadance.nextcloud;
+  nxCfg = config.services.nextcloud;
+  ngCfg = config.services.nginx.virtualHosts."${nxCfg.hostName}";
+in
+{
+  options.cadance.nextcloud = with lib; {
+    enable = mkEnableOption "Enable CADANCE Nextcloud";
 
-    package = mkOption {
-      type = types.package;
-      description = "Which package to use for the Nextcloud instance.";
-      relatedPackages = [ "nextcloud22" "nextcloud23" ];
-    };
-
-    containerName = mkOption {
+    hostName = mkOption {
       type = types.str;
-      default = "nextcloud";
-    };
-
-    hostAddress = mkOption {
-      type = types.str;
-    };
-
-    localAddress = mkOption {
-      type = types.str;
-    };
-
-    virtualHost = mkOption {
-      type = types.str;
-    };
-
-    stateDir = mkOption {
-      type = types.str;
-      default = "/var/lib/nextcloud";
-    };
-
-    uid = mkOption {
-      type = types.int;
-    };
-
-    gid = mkOption {
-      type = types.int;
+      example = "nextcloud.example.com";
     };
 
     adminpassFile = mkOption {
       type = types.str;
     };
-
-    extraConfig = {
-      dbtype = mkOption {
-        type = types.enum [ "sqlite" "pgsql" "mysql" ];
-        default = "sqlite";
-        description = "Database type.";
-      };
-      dbname = mkOption {
-        type = types.nullOr types.str;
-        default = "nextcloud";
-        description = "Database name.";
-      };
-      dbuser = mkOption {
-        type = types.nullOr types.str;
-        default = "nextcloud";
-        description = "Database user.";
-      };
-      dbpassFile = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = ''
-          The full path to a file that contains the database password.
-        '';
-      };
-      dbhost = mkOption {
-        type = types.nullOr types.str;
-        default = "localhost";
-        description = ''
-          Database host.
-          Note: for using Unix authentication with PostgreSQL, this should be
-          set to <literal>/run/postgresql</literal>.
-        '';
-      };
-      dbport = mkOption {
-        type = with types; nullOr (either int str);
-        default = null;
-        description = "Database port.";
-      };
-    };
   };
 
   config = lib.mkIf cfg.enable {
-    users.groups.nextcloud.gid = cfg.gid;
-    users.users.nextcloud = {
-      # NB: This needs both otherwise it errors...
-      isSystemUser = true;
-      isNormalUser = false;
-      group        = "nextcloud";
-      uid          = cfg.uid;
-      home         = cfg.stateDir;
-    };
+    services.nextcloud = let
+      nextcloud = pkgs.nextcloud31;
+      apps = nextcloud.packages.apps;
+    in {
+      enable = true;
+      https = true;
+      hostName = cfg.hostName;
+      package = nextcloud;
 
-    networking.nat.internalInterfaces   = [ "ve-${cfg.containerName}" ];
-    networking.networkmanager.unmanaged = [ "interface-name:ve-${cfg.containerName}" ];
-
-    containers.${cfg.containerName} = {
-      autoStart      = true;
-      ephemeral      = true;
-      privateNetwork = true;
-      hostAddress    = cfg.hostAddress;
-      localAddress   = cfg.localAddress;
-      timeoutStartSec = "10min";
-
-      bindMounts."/adminpass" = {
-        hostPath   = cfg.adminpassFile;
-        isReadOnly = true;
+      extraAppsEnable = true;
+      extraApps = {
+        inherit (apps)
+          bookmarks
+          calendar
+          contacts
+          impersonate
+          maps
+          notes
+          tasks
+          deck
+          user_oidc
+        ;
       };
 
-      bindMounts."/var/lib/nextcloud" = {
-        hostPath   = cfg.stateDir;
-        isReadOnly = false;
+      enableImagemagick = true;
+
+      maxUploadSize = "16G";
+
+      config = {
+        dbtype = "pgsql";
+        dbname = "nextcloud";
+        dbuser = "nextcloud";
+        dbhost = "/var/run/postgresql";
+
+        adminpassFile = cfg.adminpassFile;
       };
 
-      config = { config, pkgs, ... }: {
-        networking.firewall.allowedTCPPorts = [ 80 ];
+      settings.trusted_proxies = [ "127.0.0.1" ];
+      settings.default_phone_region = "AU";
 
-        services.nextcloud = {
-          enable   = true;
-          https    = true;
-          hostName = cfg.virtualHost;
-          package  = cfg.package;
-          config = cfg.extraConfig // {
-            adminpassFile = "/adminpass";
-          };
-          enableBrokenCiphersForSSE = false;
-        };
+      settings."overwrite.cli.url" = "https://${nxCfg.hostName}";
 
-        systemd.services.nextcloud-setup = {
-          unitConfig.RequiresMountsFor = ["/var/lib/nextcloud" ];
-          wants = [ "network-online.target" ];
-        };
+      settings."preview_ffmpeg_path" = "${pkgs.ffmpeg-headless}/bin/ffmpeg";
 
-        users.groups.nextcloud.gid = cfg.gid;
-        users.users.nextcloud.uid  = cfg.uid;
+      settings.allow_local_remote_servers = true;
 
-        system.stateVersion = "21.05";
+      settings.user_oidc = {
+        default_token_endpoint_auth_method = "client_secret_post";
+      };
+
+      phpOptions = {
+        "opcache.interned_strings_buffer" = "32";
       };
     };
 
-    services.caddy.virtualHosts.${cfg.virtualHost}.extraConfig = ''
-      reverse_proxy http://${cfg.localAddress}:80
+    systemd.services.nextcloud-setup = {
+      unitConfig.RequiresMountsFor = [ "/var/lib/nextcloud" ];
+      wants = [ "network-online.target" ];
+    };
+
+    services.phpfpm.pools.nextcloud.settings = {
+      "listen.owner" = config.services.caddy.user;
+      "listen.group" = config.services.caddy.group;
+    };
+    users.groups.nextcloud.members = [ "nextcloud" config.services.caddy.user ];
+
+    systemd.services.phpfpm-nextcloud = {
+      # confinement.enable = true;
+      # confinement.mode = "full-apivfs";
+      # confinement.configureNetworking = true;
+
+      serviceConfig = {
+        RemoveIPC = true;
+        CapabilityBoundingSet = [ "CAP_SETGID" "CAP_SETUID" "CAP_CHOWN" ];
+        ProtectClock = true;
+        ProtectKernelLogs = true;
+        LockPersonality = true;
+        MemoryDenyWriteExecute = false; # For PCRE JIT
+        ProtectHostname = true;
+        NoNewPrivileges = true;
+        PrivateTmp = true;
+        ProtectProc = "invisible";
+        ProtectHome = true;
+        ProtectControlGroups = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        # RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6"; # Set by the NixOS module
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallFilter = [ "@system-service" "~@privileged" "@chown" "@setuid" ];
+        SystemCallArchitectures = "native";
+
+        TemporaryFileSystem = [ "/storage" "/data" ];
+
+        # # Incompatible with confinement.
+        # ProtectSystem = lib.mkForce false;
+        # PrivateUsers = false;
+
+        # BindReadOnlyPaths = [
+        #   "/etc/passwd"
+        #   "/etc/group"
+        #   "${nxCfg.package}"
+        # ];
+
+        # BindPaths = [
+        #   nxCfg.home
+        # ];
+      };
+    };
+
+    services.caddy.virtualHosts."${cfg.hostName}".extraConfig = ''
+      # Enable gzip but do not remove ETag headers
+      encode {
+        zstd
+        gzip 4
+
+        minimum_length 256
+
+        match {
+          header Content-Type application/atom+xml
+          header Content-Type application/javascript
+          header Content-Type application/json
+          header Content-Type application/ld+json
+          header Content-Type application/manifest+json
+          header Content-Type application/rss+xml
+          header Content-Type application/vnd.geo+json
+          header Content-Type application/vnd.ms-fontobject
+          header Content-Type application/wasm
+          header Content-Type application/x-font-ttf
+          header Content-Type application/x-web-app-manifest+json
+          header Content-Type application/xhtml+xml
+          header Content-Type application/xml
+          header Content-Type font/opentype
+          header Content-Type image/bmp
+          header Content-Type image/svg+xml
+          header Content-Type image/x-icon
+          header Content-Type text/cache-manifest
+          header Content-Type text/css
+          header Content-Type text/plain
+          header Content-Type text/vcard
+          header Content-Type text/vnd.rim.location.xloc
+          header Content-Type text/vtt
+          header Content-Type text/x-component
+          header Content-Type text/x-cross-domain-policy
+        }
+      }
+
+      header Referrer-Policy "no-referrer"
+      header Strict-Transport-Security "max-age=15768000; includeSubDomains; preload;"
+      header X-Content-Type-Options "nosniff"
+      header X-Download-Options "noopen"
+      header X-Frame-Options "SAMEORIGIN"
+      header X-Permitted-Cross-Domain-Policies "none"
+      header X-Robots-Tag "noindex, nofollow"
+      header X-XSS-Protection "1; mode=block"
+
+      redir /.well-known/carddav   /remote.php/dav/ 301
+      redir /.well-known/caldav    /remote.php/dav/ 301
+      redir /.well-known/webfinger /index.php/.well-known/webfinger 301
+      redir /.well-known/nodeinfo  /index.php/.well-known/nodeinfo 301
+
+      @store_apps path_regexp ^/store-apps
+      root @store_apps ${nxCfg.home}
+
+      root * ${ngCfg.root}
+
+      @davClnt {
+        header_regexp User-Agent ^DavClnt
+        path /
+      }
+
+      redir @davClnt /remote.php/webdev{uri} 302
+
+
+      @sensitive {
+        # ^/(?:build|tests|config|lib|3rdparty|templates|data)(?:$|/)
+        path /build     /build/*
+        path /tests     /tests/*
+        path /config    /config/*
+        path /lib       /lib/*
+        path /3rdparty  /3rdparty/*
+        path /templates /templates/*
+        path /data      /data/*
+
+        # ^/(?:\.|autotest|occ|issue|indie|db_|console)
+        path /.*
+        path /autotest*
+        path /occ*
+        path /issue*
+        path /indie*
+        path /db_*
+        path /console*
+      }
+      respond @sensitive 404
+
+      @legacy {
+          path */ajax/*
+      }
+      rewrite @legacy /index.php{path}
+
+      php_fastcgi unix/${config.services.phpfpm.pools.nextcloud.socket} {
+        env modHeadersAvailable true      # Avoid sending the security headers twice
+        env front_controller_active true  # Enable pretty urls
+
+        capture_stderr
+      }
+      file_server
     '';
   };
 }
