2b74574075be02be3577f34ff4667ee25296e7bd
Zane van Iperen <zane@zanevaniperen.com>
2026-03-14 01:42:56 +1000

    modules/home/osx/hammerspoon: update config

--- modules/home/osx/hammerspoon/init.lua
+++ modules/home/osx/hammerspoon/init.lua
@@ -1,19 +1,20 @@
-function iterm_new()
-    if hs.application.find("iTerm") then
-        hs.applescript.applescript([[
-            tell application "iTerm2"
-                create window with default profile
-            end tell
-        ]])
+GHOSTTY = "/Applications/Ghostty.app"
+
+hs.hotkey.bind({"ctrl", "alt"}, "l", hs.caffeinate.lockScreen)
+hs.hotkey.bind({"ctrl", "cmd"}, "l", hs.caffeinate.lockScreen)
+
+local function openNewGhostty()
+    local ghostty = hs.application.find("Ghostty")
+    if ghostty then
+        ghostty:selectMenuItem({"File", "New Window"})
     else
-        hs.application.open("iTerm")
+        hs.application.open(GHOSTTY)
     end
 end
 
-hs.hotkey.bind({"ctrl", "alt"}, "t", iterm_new);
-hs.hotkey.bind({"ctrl", "cmd"}, "t", iterm_new);
-hs.hotkey.bind({"ctrl", "alt"}, "l", hs.caffeinate.lockScreen)
-hs.hotkey.bind({"ctrl", "cmd"}, "l", hs.caffeinate.lockScreen)
+hs.hotkey.bind({"cmd", "shift"}, "T", openNewGhostty)
+hs.hotkey.bind({"ctrl", "alt"}, "T", openNewGhostty)
+hs.hotkey.bind({"ctrl", "cmd"}, "T", openNewGhostty)
 
 function open_finder()
     hs.execute("open $HOME")
