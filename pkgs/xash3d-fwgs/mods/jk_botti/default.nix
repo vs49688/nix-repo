54475bd39f860a6e9774a24baed4164423a2515b
Zane van Iperen <zane@zanevaniperen.com>
2026-04-22 22:36:52 +1000

    xash3d-fwgs/mods/jk_botti: unstable-2026-04-21-0 -> unstable-2026-04-22-0

--- pkgs/xash3d-fwgs/mods/jk_botti/default.nix
+++ pkgs/xash3d-fwgs/mods/jk_botti/default.nix
@@ -5,14 +5,14 @@
 }:
 stdenv.mkDerivation(finalAttrs: {
   pname = "jk_botti-fwgs";
-  version = "unstable-2026-04-21-0";
+  version = "unstable-2026-04-22-0";
 
   src = fetchFromForgejo {
     domain = "git.vs49688.net";
     owner = "zane";
     repo = "jk_botti";
-    rev = "1a364d3142a383fce278d6f8ead9fc2da6b2beda";
-    hash = "sha256-VRylyJKJzt/Y7DQ3JfEDBZfEdnBXrtuoAYUTJrNQJb8=";
+    rev = "215b2c48ba2b21aac27d18156622fade1a584c36";
+    hash = "sha256-kPPdNCBZ1I4pvyEUjN8RDCIBLiZ4bK2cp/5nFQ1A3Gw=";
   };
 
   prePatch = ''
