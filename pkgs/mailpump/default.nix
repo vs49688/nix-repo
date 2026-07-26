b14d623801a43cd17548b5caab588d8168e696b1
Zane van Iperen <zane@zanevaniperen.com>
2026-07-27 03:04:36 +1000

    mailpump: cleanup derivation

--- pkgs/mailpump/default.nix
+++ pkgs/mailpump/default.nix
@@ -1,12 +1,12 @@
 { buildGoModule, lib, fetchFromForgejo }:
-buildGoModule rec {
+buildGoModule(finalAttrs: {
   pname = "mailpump";
   version = "0-unstable-2026-07-26";
 
   src = fetchFromForgejo {
     domain = "git.vs49688.net";
     owner = "zane";
-    repo = pname;
+    repo = "mailpump";
     rev = "eeb26a9d6efb0e8d90f435f46cb9fcd6c94b8588";
     hash = "sha256-UidANkxcc+h9GSKCKKKdpyl4au3ouso18bBEudjZhu0=";
   };
@@ -14,10 +14,11 @@ buildGoModule rec {
   vendorHash = null;
 
   meta = with lib; {
+    mainProgram = "mailpump";
     description = "A service that monitors a mailbox for messages and will automatically move them to another, usually on a different server";
-    homepage = "https://github.com/vs49688/mailpump";
+    homepage = "https://git.vs49688.net/zane/mailpump";
     license = licenses.gpl2Only;
     platforms = platforms.all;
     maintainers = with maintainers; [ zane ];
   };
-}
+})
