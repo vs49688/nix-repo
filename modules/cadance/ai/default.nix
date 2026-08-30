ae7c9a64f94b356da033a83a71e7543205fb8986
Zane van Iperen <zane@zanevaniperen.com>
2026-08-30 14:55:28 +1000

    Revert "modules/cadance/ai/litellm: add expression dependency"
    This reverts commit 77971f2114da20e1f380f80d8f3ca3a7313cf394.

--- modules/cadance/ai/default.nix
+++ modules/cadance/ai/default.nix
@@ -109,8 +109,6 @@ in
 
     services.litellm = let
       xpkgs = pkgs.python3Packages.overrideScope (final: prev: {
-        expression = pkgs.python3Packages.callPackage ./expression.nix { };
-
         litellm = prev.litellm.overridePythonAttrs(old: rec {
           version = "1.96.2";
 
@@ -140,7 +138,6 @@ in
           postPatch = "";
           dependencies =
             (old.dependencies or [ ])
-            ++ [ final.expression ]
             ++ prev.litellm.optional-dependencies.proxy
             ++ prev.litellm.optional-dependencies.extra_proxy
             ++ prev.litellm.optional-dependencies.proxy-runtime;
